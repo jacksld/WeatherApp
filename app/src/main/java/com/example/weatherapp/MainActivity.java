84aa9953cd2e545b047744ec09d69f0c486c122a
jack <acesandwinners.14@gmail.com>
2025-02-26 23:08:15 +0700

    26-2

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -7,18 +7,83 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.os.*;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
+    TextView cityName;
+    Button button;
+    TextView weatherInfo;
+    String url;
+
+    class getWeather extends AsyncTask<String, void, String> {
+        @Override
+        protected String doInBackground(String... url) {
+            StringBuilder result = new StringBuilder();
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        }); */
+        cityName = findViewById(R.id.cityName);
+        button = findViewById(R.id.button);
+        weatherInfo = findViewById(R.id.weatherInfo);
+
+        final String[] temp={""};
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
+                String city = cityName.getText().toString();
+                try {
+                    if (city != null) {
+                        url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5c6d80e979d05fd4cd517d6753af8e45";
+                    } else {
+                        Toast.makeText(MainActivity.this, "Please enter a city name", Toast.LENGTH_SHORT).show();
+                    }
+                    getWeather task = new getWeather();
+                    temp[0] = task.execute(url).get();
+                } catch(ExecutionException e) {
+                    e.printStackTrace();
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (temp == null) {
+                    show.setText("Unable to find weather");
+                }
+
+
+            }
         });
+
     }
 }
